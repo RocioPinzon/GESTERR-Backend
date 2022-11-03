a53ed3c8aae4d26a1639e38d37bd1e4555caf7eb
RocioPinzon <pinzonbayonr@gmail.com>
2022-11-03 23:20:47 +0100

    GesTerr | Creacion y configuracion basica de proyecto parte backend. Nodejs, express, morgan y nodemon

---
+++ src/app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const path  = require('path');
+const morgan  = require('morgan');
+
+// Initiliazations // 
+const app = express();
+
+// Settings // 
+app.set('port', process.env.PORT || 3000);
+app.set(express(express.json()));
+
+
+
+// Middlewares // 
+app.use(morgan('dev'));
+
+
+
+// Global Variables  // 
+
+
+// Routes // 
+
+
+// Static files // 
+
+
+app.use(express.static(path.join(__dirname,'public')));
+
+// Server is listenning  // 
+app.listen(app.get('port'), () =>{
+    console.log('Servidor en puerto', app.get('port'))
+});
+
+console.log("Bienvenido a GesTerr");
