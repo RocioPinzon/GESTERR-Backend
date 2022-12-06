2976f278913c4e07b577fdec6fc7000e34cfb9ca
RocioPinzon <pinzonbayonr@gmail.com>
2022-12-06 17:04:23 +0100

    GesTerr | Actualizacion

--- src/models/Producto.js
+++ src/models/Producto.js
@@ -7,7 +7,7 @@ const ProductoSchema = new Schema({
     cantidad: {type: String},
     user:{type:String},
     cultivoId: {type: String},
-    precio:{type: mongoose.Decimal128}
+    precio:{type: Number}
 });
 
 
