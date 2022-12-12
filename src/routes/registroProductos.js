2f0fdf14bd7d93433d811dc5d4b3a163885a1bf8
RocioPinzon <pinzonbayonr@gmail.com>
2022-12-12 22:18:55 +0100

    GesTerr | Actualizacion

--- src/routes/registroProductos.js
+++ src/routes/registroProductos.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const ProductoCultivo = require('../models/RegistroProducto');
 
 router.get('/:idUser/campos/:idCampo/cultivos/:idCultivo/registroproductos', async (req,res) => { //obtener registro productos de un cutlivo
-    console.log("Obtener cultivos: " +JSON.stringify(req.params));
+    console.log("Obtener regsitros: " +JSON.stringify(req.params));
     //necestas el id del usuario - necesito req.params.idUser
     const registroproductos = await ProductoCultivo.find({cultivoId: req.params.idCultivo});
     console.log("registroproductos --> " + registroproductos);
@@ -16,13 +16,19 @@ router.post('/:idUser/campos/:idCampo/cultivos/:idCultivo/registroproductos', as
     console.log("req.body --> " , JSON.stringify(req.body));
     const registroProducto = new ProductoCultivo(req.body);
     registroProducto.cultivoId = req.params.idCultivo;
-    registroProducto.productoId = req.params.idCampo;
+    registroProducto.productoId = req.params.productoId;
 
     //necestas el id del usuario - necesito req.params.idUser
     //console.log("req.body --> " , req.body);
     //console.log("Cultivos() --> " + new Cultivos());
     const registroCreado = await registroProducto.save();
     res.json(registroCreado);
+});
+
+router.delete('/:idUser/campos/:idCampo/cultivos/:idCultivo/registroproductos/:idRegistro', async(req,res)=>{
+
+    const registroproducto = await ProductoCultivo.findByIdAndDelete(req.params.idRegistro, req.body);
+    res.json(registroproducto);
 });
 
 module.exports = router;
